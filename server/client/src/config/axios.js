7d2f3e7d71d2f0487f7cac13f7f4de7a1fe40024
Flare_MozZ <manojsaivardhan313@gmail.com>
2025-04-05 15:30:47 +0530

    host temp

--- server/client/src/config/axios.js
+++ server/client/src/config/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  // baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "https://auditions-lc-v3qz.onrender.com",
   withCredentials: true,
 });
 
